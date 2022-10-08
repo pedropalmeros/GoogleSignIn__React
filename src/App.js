1591f9b89a85e7b0a01a45b20a298d9f5c06dd33
pedropalmeros <pedrofpalmeros@gmail.com>
2022-10-08 12:35:32 -0500

    [ADD] Google Sign In and interaction with React and the Backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  function handleCallbackResponse (response){
+    console.log(response);
+    console.log("Encoded JWT ID token: " + response.credential);
+
+    const body = {id_token: response.credential};
+
+    fetch('http://localhost:8080/auth/google',{
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+
+  }
+
+  useEffect(()=>{
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "160235058947-42ri2mstjjl5epb49k68rau12sal41jp.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      {theme: "outline", size: "large"}
+    )
+  },[]);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
